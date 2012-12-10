8b85ae00dae52b9d3aa4be4dfd5800c82e8c6d24
Evelyn Hung <jj.evelyn@gmail.com>
2012-12-11 01:29:47 +0800

    add sound and margin

--- apps/bubblewrapper/js/bubble.js
+++ apps/bubblewrapper/js/bubble.js
@@ -1,19 +1,42 @@
+const CLICK_SOUND = 'data:audio/x-wav;base64,' +
+  'UklGRiADAABXQVZFZm10IBAAAAABAAEAIlYAAESsAAACABAAZGF0YfwCAAAW/Fzsqe9O' +
+  'AONWB0Pt3Mf1hsS38mJcc0mq9mzpwsIwsChOBxay/ikHV6Tr8ioJNQa0ErvFzbXrw97j' +
+  '5C2LQII7aBg77Tr+I+wH0QWp/7xowHegIf0yD1UkhzRRIbUGoeOgJptCHVB+WZg5ehgs' +
+  'EcofKwKaAb7+cuzd9doICAx0FZEm+gEq+z//D/yJDtEJx/O73MHkifPK/BoLXwwuBt3p' +
+  '5eBq2h3YT/OR+MH/5xDGB7sHowyp9rrrL++06mnt/PpcALcI7RDSCz4GwwWaAXYNVhLw' +
+  'D20VYQsvCWUPxApJCVUH3P0jA54EIP0RBUYHVgtlD68KtQWI/9MB4f8Q/Fr4UvLz7nPq' +
+  'yOzV9AvzKfEB7azl/+ee6jbrSOw16mjpPepD7d3yT/hL/RIDBAXQAHcDIAZ1BVsPIhAZ' +
+  'CT4Ntwc2CJsQnhV+GlYcJR67GF0WaRK5CewGSQdSBboCfgWGBaQACP0e+8f3O/Y4+Yn1' +
+  '4e8l9Mf3lvns/eT75fbx9t359/lw+6L+XP+5AdsFSgZECK8LvQlVCWYJ1wetBD8AGALl' +
+  'AJUAVAbPBEkDpALfADn/Cv4c/+7+OP/jAAb/7vie+Xr7GvYa9g30rPBc9OL1wveo+3D+' +
+  '8/xG+Zn5tPsi/vX/xv4I/Oj5DPaL8mbxmfMM+80AXQbiCisNvhC8Dt4LGwwyDJkNlAxR' +
+  'CWYGswcHCn0KyA5cDsQKYgrZB+cFlATlAh4A3P5kAOsAOwLbA+ED8gLAAM/+h/vq+Lb5' +
+  'qPgY+GH5i/nE+SX6V/s9+gv69vl89nv33fhc+Zb6nvse/lEA4wMjBrQEugPc/4/8pvux' +
+  '+//9Kf9tAGcBXAFxAtgCuwMeBFQE6AQdA4gCGAJiADsAuwC7/53+a/4J/tv88fte+R74' +
+  'dPhd+HD5LPmf+If5VPsp/noASALRAbsB+wJ+Ak0CuQPiBAsFpwYTB5wFtgZ/DE4P8AuH' +
+  'B4kD3QKPBcAHhgaHBDAEngO6BBcFbwJ2/qD7rPtG/voBwQGU/pn9Lv3T/g==';
+
 var BubbleWrapper = {
   ROW: 7,
   COL: 5,
+  SIZE: 50,
+  margin: 0,
   panel: document.getElementById('panel'),
 
   init: function() {
     for(var i=0; i<this.ROW; ++i) {
-      this.makeOneRow();
+      this.makeOneRow(i);
     }
   },
 
-  makeOneRow: function() {
+  makeOneRow: function(rId) {
     var aRow = document.createElement('div');
-    for(var i=0; i<this.COL; ++i) {
+    aRow.classList.add('row');
+    if (rId%2)
+      aRow.classList.add('shift');
+    for(var i = 0; i < this.COL; ++i) {
       var aCol = document.createElement('span');
-      aCol.className = 'bubble';
+      aCol.classList.add('bubble');
       aCol.onclick = this.brokenClick;
 
       aRow.appendChild(aCol);
@@ -26,6 +49,8 @@ var BubbleWrapper = {
   brokenClick: function(evt) {
     var aCol = evt.target;
     aCol.onclick = null;
+    var clicker = new Audio(CLICK_SOUND);
+    clicker.play();
     var brokenNumber = Math.round(Math.random() * 3 + 1);
     aCol.className = 'bubble-broken-' + brokenNumber;
   }
